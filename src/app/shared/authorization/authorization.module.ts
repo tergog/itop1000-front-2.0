371e8de99201f2cd0264b42ff56c8cbd322533bf
Vlad Ivashchenko <mr.ruryks@gmail.com>
2021-07-15 10:09:47 +0300

    init authorization && landing components

---
+++ src/app/shared/authorization/authorization.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthorizationComponent } from './component/authorization.component';
+
+
+
+@NgModule({
+  declarations: [
+    AuthorizationComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthorizationModule { }
---
+++ src/app/shared/landing/landing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LandingComponent } from './component/landing.component';
+
+
+
+@NgModule({
+  declarations: [
+    LandingComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LandingModule { }
