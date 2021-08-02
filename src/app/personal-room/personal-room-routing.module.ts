fec472a9197a10d01b49a3098073dfe1317969e0
Vlad Ivashchenko <mr.ruryks@gmail.com>
2021-08-02 17:51:53 +0300

    init personal-room routing

--- src/app/personal-room/personal-room-routing.module.ts
+++ src/app/personal-room/personal-room-routing.module.ts
@@ -15,6 +15,42 @@ const routes: Routes = [
       {
         path: 'home',
         loadChildren: () => import('./children/home/home.module').then(m => m.HomeModule)
+      },
+      {
+        path: 'wallet',
+        loadChildren: () => import('./children/wallet/wallet.module').then(m => m.WalletModule)
+      },
+      {
+        path: 'projects',
+        loadChildren: () => import('./children/projects/projects.module').then(m => m.ProjectsModule)
+      },
+      {
+        path: 'works',
+        loadChildren: () => import('./children/works/works.module').then(m => m.WorksModule)
+      },
+      {
+        path: 'tracker',
+        loadChildren: () => import('./children/tracker/tracker.module').then(m => m.TrackerModule)
+      },
+      {
+        path: 'chat',
+        loadChildren: () => import('./children/chat/chat.module').then(m => m.ChatModule)
+      },
+      {
+        path: 'security',
+        loadChildren: () => import('./children/security/security.module').then(m => m.SecurityModule)
+      },
+      {
+        path: 'developers',
+        loadChildren: () => import('./children/developers/developers.module').then(m => m.DevelopersModule)
+      },
+      {
+        path: 'contracts',
+        loadChildren: () => import('./children/contracts/contracts.module').then(m => m.ContractsModule)
+      },
+      {
+        path: '**',
+        redirectTo: 'home'
       }
     ]
   }
